c9fbbf6b1a3490fb70e46e9ae8d7466a8fc38603
Herval Mata <hervalmataamparo@gmail.com>
2021-11-07 13:10:19 -0300

    feat:art: api - register container for users tokens repository

--- api/src/shared/container/index.ts
+++ api/src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import {container} from "tsyringe";
 import {IUsersRepository} from "../../modules/users/repositories/IUsersRepository";
 import {UsersRepository} from "../../modules/users/repositories/implementations/UsersRepository";
+import {IUsersTokensRepository} from "../../modules/users/repositories/IUsersTokensRepository";
+import {UsersTokensRepository} from "../../modules/users/repositories/implementations/UsersTokensRepository";
 
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+    "UsersTokensRepository",
+    UsersTokensRepository
 );
