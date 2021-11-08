1e5ec441216cdfd5aa1d20492a7f7cb57f77c10c
Herval Mata <hervalmataamparo@gmail.com>
2021-11-08 15:56:16 -0300

    feat:art: api - created migration for users profile

---
+++ api/src/shared/infra/typeorm/migrations/1636396965213-CreateUsersProfile.ts
@@ -0,0 +1,62 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsersProfile1636396965213 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users_profile",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "user_id",
+                        type: "uuid",
+                    },
+                    {
+                        name: "cpf",
+                        type: "varchar",
+                        isUnique: true,
+                    },
+                    {
+                        name: "birth_date",
+                        type: "varchar",
+                        isUnique: true,
+                    },
+                    {
+                        name: "phone_number",
+                        type: "varchar",
+                    },
+                    {
+                        name: "avatar",
+                        type: "varchar",
+                        isNullable: true,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        name: "FK_Users_Profiles_User",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users_profile");
+    }
+
+}
