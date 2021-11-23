c8429d7d2a34cfa2c733409c523a26a27f9056d1
Herval Mata <hervalmataamparo@gmail.com>
2021-11-23 14:21:03 -0300

    feat:art: api - refactor service test for update a user order

--- api/src/modules/orders/services/UpdateStatusOrderService.spec.ts
+++ api/src/modules/orders/services/UpdateStatusOrderService.spec.ts
@@ -10,9 +10,16 @@ import {OrdersRepositoryInMemory} from "../repositories/in-memory/OrdersReposito
 import {CreateOrderService} from "./CreateOrderService";
 import {UpdateStatusOrderService} from "./UpdateStatusOrderService";
 import {DayjsDateProvider} from "../../../shared/container/providers/DateProvider/implementations/DayjsDateProvider";
-import {Type} from "../../deliveries/entities/Delivery";
-import {Method} from "../../payments/entities/Payment";
-import {StatusOrder} from "../entities/Order";
+import {StatusOrderRepositoryInMemory} from "../repositories/in-memory/StatusOrderRepositoryInMemory";
+import {TypesDeliveryRepositoryInMemory} from "../../deliveries/repositories/in-memory/TypesDeliveryRepositoryInMemory";
+import {MethodsPaymentRepositoryInMemory} from "../../payments/repositories/in-memory/MethodsPaymentRepositoryInMemory";
+import {StatusPaymentRepositoryInMemory} from "../../payments/repositories/in-memory/StatusPaymentRepositoryInMemory";
+import {StatusDeliveryRepositoryInMemory} from "../../deliveries/repositories/in-memory/StatusDeliveryRepositoryInMemory";
+import {CreateStatusDeliveryService} from "../../deliveries/services/CreateStatusDeliveryService";
+import {CreateStatusPaymentService} from "../../payments/services/CreateStatusPaymentService";
+import {CreateStatusOrderService} from "./CreateStatusOrderService";
+import {CreateMethodsPaymentService} from "../../payments/services/CreateMethodsPaymentService";
+import {CreateTypesDeliveryService} from "../../deliveries/services/CreateTypesDeliveryService";
 
 let usersRepositoryInMemory: UsersRepositoryInMemory;
 let createUserService: CreateUserService;
@@ -22,6 +29,16 @@ let productsRepositoryInMemory: ProductsRepositoryInMemory;
 let createProductService: CreateProductService;
 let deliveriesRepositoryInMemory: DeliveriesRepositoryInMemory;
 let paymentsRepositoryInMemory: PaymentsRepositoryInMemory;
+let statusOrderRepositoryInMemory: StatusOrderRepositoryInMemory;
+let typesDeliveryRepositoryInMemory: TypesDeliveryRepositoryInMemory;
+let methodsPaymentRepositoryInMemory: MethodsPaymentRepositoryInMemory;
+let statusPaymentRepositoryInMemory: StatusPaymentRepositoryInMemory;
+let statusDeliveryRepositoryInMemory: StatusDeliveryRepositoryInMemory;
+let createStatusDeliveryService: CreateStatusDeliveryService;
+let createStatusPaymentService: CreateStatusPaymentService;
+let createStatusOrderService: CreateStatusOrderService;
+let createMethodsPaymentService: CreateMethodsPaymentService;
+let createTypesDeliveryService: CreateTypesDeliveryService;
 let ordersRepositoryInMemory: OrdersRepositoryInMemory;
 let dateProvider: DayjsDateProvider;
 let createOrderService: CreateOrderService;
@@ -38,12 +55,24 @@ describe('Update Status Order', () => {
         deliveriesRepositoryInMemory = new DeliveriesRepositoryInMemory();
         paymentsRepositoryInMemory = new PaymentsRepositoryInMemory();
         ordersRepositoryInMemory = new OrdersRepositoryInMemory();
+        typesDeliveryRepositoryInMemory = new TypesDeliveryRepositoryInMemory();
+        statusDeliveryRepositoryInMemory = new StatusDeliveryRepositoryInMemory();
+        methodsPaymentRepositoryInMemory = new MethodsPaymentRepositoryInMemory();
+        statusPaymentRepositoryInMemory = new StatusPaymentRepositoryInMemory();
+        statusOrderRepositoryInMemory = new StatusOrderRepositoryInMemory();
+        createStatusDeliveryService = new CreateStatusDeliveryService(statusDeliveryRepositoryInMemory);
+        createMethodsPaymentService = new CreateMethodsPaymentService(methodsPaymentRepositoryInMemory);
+        createStatusPaymentService = new CreateStatusPaymentService(statusPaymentRepositoryInMemory);
+        createTypesDeliveryService = new CreateTypesDeliveryService(typesDeliveryRepositoryInMemory);
+        createStatusOrderService = new CreateStatusOrderService(statusOrderRepositoryInMemory);
         dateProvider = new DayjsDateProvider();
         createOrderService = new CreateOrderService(
             ordersRepositoryInMemory, deliveriesRepositoryInMemory,
             paymentsRepositoryInMemory, productsRepositoryInMemory,
+            statusDeliveryRepositoryInMemory,
+            statusPaymentRepositoryInMemory, statusOrderRepositoryInMemory
         );
-        updateStatusOrderService = new UpdateStatusOrderService(ordersRepositoryInMemory, dateProvider);
+        updateStatusOrderService = new UpdateStatusOrderService(ordersRepositoryInMemory, dateProvider, statusOrderRepositoryInMemory);
     });
 
     it('should be able to update an status order', async () => {
@@ -91,17 +120,40 @@ describe('Update Status Order', () => {
         const taxes = 2;
         const total = 12;
         const products_id = [product1_id, product2_id];
+        const type_delivery = "post_offices";
+        await createTypesDeliveryService.execute({type_delivery});
+        const types_delivery = await typesDeliveryRepositoryInMemory.findByTypeDelivery("post_offices");
+        const type_delivery_id = types_delivery.id;
+        const status_delivery = "packaging";
+        await createStatusDeliveryService.execute({status_delivery});
+        const statusDelivery = await statusDeliveryRepositoryInMemory.findByStatusDelivery("packaging");
+        const status_delivery_id = statusDelivery.id;
+        const status_payment = "pending";
+        await createStatusPaymentService.execute({status_payment});
+        const statusPayment = await statusPaymentRepositoryInMemory.findByStatusPayment("pending");
+        const status_payment_id = statusPayment.id;
+        const status_order = "pending";
+        await createStatusOrderService.execute({status_order});
+        const statusOrder = await statusOrderRepositoryInMemory.findByStatusOrder("pending");
+        const status_order_id = statusOrder.id;
+        const method = "pix";
+        await createMethodsPaymentService.execute({method: method});
+        const methods = await methodsPaymentRepositoryInMemory.findByMethodPayment("pix");
+        const method_id = methods.id;
         await createOrderService.execute({
-            type: Type.post_offices,
+            type_delivery_id: type_delivery_id,
             cost: cost, prize: prize, street: street, number: number,
             district: district, postal_code: postal_code, city: city, state: state, country: country,
-            method: Method.pix,
+            method_id: method_id,
             value: value, installments: installments, taxes: taxes,
             total: total, user_id: user_id, products_id
         });
         const order = await ordersRepositoryInMemory.findByUser(user_id);
         const order_unique = order[Object.keys(order)[0]];
         const order_id = order_unique.id;
-        await expect(updateStatusOrderService.execute({ id: order_id, status: StatusOrder.finished})).resolves.not.toThrow();
+        await expect(updateStatusOrderService.execute({
+            id: order_id,
+            status_order_id: status_order_id
+        })).resolves.not.toThrow();
     });
 });
--- api/src/modules/orders/services/UpdateStatusOrderService.ts
+++ api/src/modules/orders/services/UpdateStatusOrderService.ts
@@ -1,11 +1,12 @@
 import {inject, injectable} from "tsyringe";
 import {IOrdersRepository} from "../repositories/IOrdersRepository";
-import {StatusOrder} from "../entities/Order";
+
 import {DayjsDateProvider} from "../../../shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import {IStatusOrderRepository} from "../repositories/IStatusOrderRepository";
 
 interface IRequest {
     id: string;
-    status: StatusOrder;
+    status_order_id: string;
 }
 
 @injectable()
@@ -15,20 +16,24 @@ class UpdateStatusOrderService {
         @inject("OrdersRepository")
         private ordersRepository: IOrdersRepository,
         @inject("DateProvider")
-        private dateProvider: DayjsDateProvider
-    ) {}
-    
-    async execute({ id, status }: IRequest): Promise<void> {
-        await this.ordersRepository.updateStatus(id, status);
+        private dateProvider: DayjsDateProvider,
+        @inject("StatusOrderRepository")
+        private statusOrderRepository: IStatusOrderRepository
+    ) {
+    }
+
+    async execute({id, status_order_id}: IRequest): Promise<void> {
+        const statusOrder = await this.statusOrderRepository.findById(status_order_id);
+        const status_order = statusOrder.status_order;
         const order = await this.ordersRepository.findById(id);
-        if (status === StatusOrder.payed) {
+        if (status_order === "payed") {
             order.payed_date = this.dateProvider.dateNow();
-        } else if (status === StatusOrder.cancelled) {
+        } else if (status_order === "cancelled") {
             order.cancelled_date = this.dateProvider.dateNow();
-        } else if (status === StatusOrder.finished) {
+        } else if (status_order === "finished") {
             order.finished_date = this.dateProvider.dateNow();
         }
-        await this.ordersRepository.create(order);
+        await this.ordersRepository.updateStatus(id, status_order_id);
     }
 }
 
