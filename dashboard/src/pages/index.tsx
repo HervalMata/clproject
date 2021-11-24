39a6bd5c4419cedbcd258d2f03e69a11a1a01f69
Herval Mata <hervalmataamparo@gmail.com>
2021-11-24 20:52:02 -0300

    feat:art: dashboard - centered element in window

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -1,5 +1,9 @@
+import {Flex} from "@chakra-ui/react";
+
 export default function Home() {
-  return (
-      <h1>Dashboard</h1>
-  )
+    return (
+        <Flex w='100vm' h='100vh' alignItems='center' justifyContent='center'>
+            <h1>Dashboard</h1>
+        </Flex>
+    )
 }
