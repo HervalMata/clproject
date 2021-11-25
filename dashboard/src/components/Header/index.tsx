b8ffe22ebcc44703e9a7be096455150e6f6d157f
Herval Mata <hervalmataamparo@gmail.com>
2021-11-25 20:01:08 -0300

    feat:art: dashboard - created component header

---
+++ dashboard/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import {Flex} from "@chakra-ui/react";
+
+export function Index() {
+    return (
+        <Flex as='header' w="100%" maxWidth={1488}
+              h='28' marginX='auto' mt='4' px='6' align='center'>
+            Header
+        </Flex>
+    )
+}
