aa0d87f22838af95fcc60221a802d7cc09cf0547
Herval Mata <hervalmataamparo@gmail.com>
2021-11-25 18:35:39 -0300

    feat:art: dashboard - using form control for separated inputs

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {Button, Flex, Input, Stack} from "@chakra-ui/react";
+import {Button, Flex, FormControl, Input, Stack} from "@chakra-ui/react";
 
 export default function Home() {
     return (
@@ -6,16 +6,20 @@ export default function Home() {
             <Flex
                 as="form" width="100%" maxWidth={360} bg='pink.600' p='8' borderRadius='8' flexDir="column">
                 <Stack spacing='4'>
-                    <Input
-                        type='email' name='email' placeholder='Email'
-                        focusBorderColor='blue.500' bgColor="gray.300"
-                        variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
-                    />
-                    <Input
-                        type='password' name='password' placeholder='Senha'
-                        focusBorderColor='blue.500' bgColor="gray.300"
-                        variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
-                    />
+                    <FormControl>
+                        <Input
+                            type='email' name='email' placeholder='Email'
+                            focusBorderColor='blue.500' bgColor="gray.300"
+                            variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <Input
+                            type='password' name='password' placeholder='Senha'
+                            focusBorderColor='blue.500' bgColor="gray.300"
+                            variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
+                        />
+                    </FormControl>
                 </Stack>
                 <Button type="submit" mt="6" colorScheme='blue'>Entrar</Button>
             </Flex>
