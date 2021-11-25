397321b548c1c3c1a98005f6b6f7b0f974f48d7e
Herval Mata <hervalmataamparo@gmail.com>
2021-11-25 18:32:45 -0300

    feat:art: dashboard - using stack for separated inputs

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -1,20 +1,22 @@
-import {Button, Flex, Input} from "@chakra-ui/react";
+import {Button, Flex, Input, Stack} from "@chakra-ui/react";
 
 export default function Home() {
     return (
         <Flex w='100vm' h='100vh' align='center' justify='center'>
             <Flex
                 as="form" width="100%" maxWidth={360} bg='pink.600' p='8' borderRadius='8' flexDir="column">
-                <Input
-                    type='email' name='email'
-                    focusBorderColor='blue.500' bgColor="gray.300"
-                    variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
-                />
-                <Input
-                    type='password' name='password'
-                    focusBorderColor='blue.500' bgColor="gray.300"
-                    variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
-                />
+                <Stack spacing='4'>
+                    <Input
+                        type='email' name='email' placeholder='Email'
+                        focusBorderColor='blue.500' bgColor="gray.300"
+                        variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
+                    />
+                    <Input
+                        type='password' name='password' placeholder='Senha'
+                        focusBorderColor='blue.500' bgColor="gray.300"
+                        variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
+                    />
+                </Stack>
                 <Button type="submit" mt="6" colorScheme='blue'>Entrar</Button>
             </Flex>
         </Flex>
