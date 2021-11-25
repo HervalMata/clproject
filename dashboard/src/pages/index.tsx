1744b9a650c5f1c35cc608e191b31a6f81e0d58b
Herval Mata <hervalmataamparo@gmail.com>
2021-11-25 18:27:47 -0300

    feat:art: dashboard - styled inputs login

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -4,9 +4,17 @@ export default function Home() {
     return (
         <Flex w='100vm' h='100vh' align='center' justify='center'>
             <Flex
-                as="form" width="100%" maxWidth={360} bg='pink.700' p='8' borderRadius='8' flexDir="column">
-                <Input type='email' name='email'/>
-                <Input type='password' name='password'/>
+                as="form" width="100%" maxWidth={360} bg='pink.600' p='8' borderRadius='8' flexDir="column">
+                <Input
+                    type='email' name='email'
+                    focusBorderColor='blue.500' bgColor="gray.300"
+                    variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
+                />
+                <Input
+                    type='password' name='password'
+                    focusBorderColor='blue.500' bgColor="gray.300"
+                    variant='filled' _hover={{bgColor: 'gray.300'}} size='lg'
+                />
                 <Button type="submit" mt="6" colorScheme='blue'>Entrar</Button>
             </Flex>
         </Flex>
