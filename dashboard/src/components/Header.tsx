022ee2ae8ed89e17781abe355d71fb2cfdd5b0f5
Herval Mata <hervalmataamparo@gmail.com>
2021-11-25 22:23:19 -0300

    feat:art: dashboard - added other icons

--- dashboard/src/components/Header.tsx
+++ dashboard/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import {Flex, Icon, Input, Text} from "@chakra-ui/react";
-import {RiSearchLine} from "react-icons/all";
+import {Flex, HStack, Icon, Input, Text} from "@chakra-ui/react";
+import {RiNotificationLine, RiSearchLine, RiUserAddLine} from "react-icons/all";
 
 export function Header() {
     return (
@@ -14,6 +14,12 @@ export function Header() {
                 <Input color='gray.50' variant='unlisted' px='4' mr='4' placeholder='Pesquisar'
                        _placeholder={{color: 'gray.400 '}}/>
                 <Icon as={RiSearchLine} fontSize='20' h='10'/>
+            </Flex>
+            <Flex align='8' ml='auto'>
+                <HStack spacing='8' mx='8' pr='8' py='1' color='gray.300' borderRightWidth={1} borderColor='gray.700'>
+                    <Icon as={RiNotificationLine} fontSize='20' h='10'/>
+                    <Icon as={RiUserAddLine} fontSize='20' h='10'/>
+                </HStack>
             </Flex>
         </Flex>
     )
